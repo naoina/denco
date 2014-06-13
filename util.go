47628cb6dccd65f29d2d7146a7012df99dda57f7
Naoya Inada <naoina@kuune.org>
2014-06-13 22:58:09 +0900

    Remove unused util functions

--- util.go
+++ util.go
@@ -10,21 +10,3 @@ func NextSeparator(path string, start int) int {
 	}
 	return start
 }
-
-// isMetaChar returns whether the meta character.
-func IsMetaChar(c byte) bool {
-	return c == ParamCharacter || c == WildcardCharacter
-}
-
-// ParamNames returns parameter names in given path.
-// It returns names which meta character is prefixed.
-func ParamNames(path string) (names []string) {
-	for i := 0; i < len(path); i++ {
-		if IsMetaChar(path[i]) {
-			next := NextSeparator(path, i+1)
-			names = append(names, path[i:next])
-			i = next
-		}
-	}
-	return names
-}
--- util_test.go
+++ util_test.go
@@ -29,32 +29,3 @@ func TestNextSeparator(t *testing.T) {
 		}
 	}
 }
-
-func TestIsMetaChar(t *testing.T) {
-	for _, c := range []byte{':', '*'} {
-		if !denco.IsMetaChar(c) {
-			t.Errorf("Expect %q is meta charcter, but isn't", c)
-		}
-	}
-	for c := byte(0); c < 0xff && c != ':' && c != '*'; c++ {
-		if denco.IsMetaChar(c) {
-			t.Errorf("Expect %q is not meta character, but isn't", c)
-		}
-	}
-}
-
-func TestParamNames(t *testing.T) {
-	for path, expected := range map[string][]string{
-		"/:a":    {":a"},
-		"/:b":    {":b"},
-		"/:a/:b": {":a", ":b"},
-		"/:ab":   {":ab"},
-		"/*w":    {"*w"},
-		"/*w/:p": {"*w", ":p"},
-	} {
-		actual := denco.ParamNames(path)
-		if !reflect.DeepEqual(actual, expected) {
-			t.Errorf("%q expects %q, but %q", path, expected, actual)
-		}
-	}
-}
