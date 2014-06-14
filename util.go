6b630e7575267fef969dddfdaa6866f2648d5bda
Naoya Inada <naoina@kuune.org>
2014-06-14 19:04:05 +0900

    route path now can specify '.' character

--- util.go
+++ util.go
@@ -3,7 +3,7 @@ package denco
 // NextSeparator returns an index of next separator in path.
 func NextSeparator(path string, start int) int {
 	for start < len(path) {
-		if c := path[start]; c == '/' || c == '.' || c == TerminationCharacter {
+		if c := path[start]; c == '/' || c == TerminationCharacter {
 			break
 		}
 		start++
--- util_test.go
+++ util_test.go
@@ -16,10 +16,10 @@ func TestNextSeparator(t *testing.T) {
 		{"/path/to/route", 0, 0},
 		{"/path/to/route", 1, 5},
 		{"/path/to/route", 9, 14},
-		{"/path.html", 1, 5},
+		{"/path.html", 1, 10},
 		{"/foo/bar.html", 1, 4},
-		{"/foo/bar.html/baz.png", 5, 8},
-		{"/foo/bar.html/baz.png", 10, 13},
+		{"/foo/bar.html/baz.png", 5, 13},
+		{"/foo/bar.html/baz.png", 14, 21},
 		{"path#", 0, 4},
 	} {
 		actual := denco.NextSeparator(testcase.path, testcase.start)
